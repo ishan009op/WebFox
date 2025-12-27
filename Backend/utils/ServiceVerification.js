3ca00158e93c5b1a6c90a786171d84b1825d0f0a
Ishan <ishanprashar97085@gmail.com>
2025-12-27 17:59:24 +0530

    check

--- Backend/utils/ServiceVerification.js
+++ Backend/utils/ServiceVerification.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
-
+import dotenv from 'dotenv'
+dotenv.config()
 export const sendVerificationEmail = async (toEmail, token) => {
   try {
     const transporter = nodemailer.createTransport({
