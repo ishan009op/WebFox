e7b37f57e14b06702833226cec6f229c03631e9d
Ishan <ishanprashar97085@gmail.com>
2025-12-27 21:38:22 +0530

    done

--- Backend/routes/User.Route.js
+++ Backend/routes/User.Route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addUser, EmailVerify, singleUser, viewUsers } from '../controllers/User.controller.js'
+import { addUser, verifyEmail, singleUser, viewUsers } from '../controllers/User.controller.js'
 import { isAdmin } from '../middlewares/Admin.middleware.js'
 
 
@@ -10,6 +10,6 @@ const router = express.Router()
 router.get('/',isAdmin,viewUsers)
 router.post('/',addUser)
 router.get('/:id',isAdmin,singleUser)
-router.get('/verify-email/:token',EmailVerify)
+router.get('/verify-email/:token',verifyEmail)
 
 export default router
