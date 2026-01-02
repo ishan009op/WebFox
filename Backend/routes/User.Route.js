0dd4b8660ccdd5ea12c9287cd2c642a2a8a9dde0
Ishan <ishanprashar97085@gmail.com>
2026-01-02 22:14:00 +0530

    done

--- Backend/routes/User.Route.js
+++ Backend/routes/User.Route.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import { addUser, verifyEmail, singleUser, viewUsers, refresh } from '../controllers/User.controller.js'
+import { addUser, singleUser, viewUsers } from '../controllers/User.controller.js'
 import { isAdmin } from '../middlewares/Admin.middleware.js'
-import { protect } from '../middlewares/Auth.middleware.js'
+
 
 
 
@@ -11,8 +11,7 @@ const router = express.Router()
 router.get('/',isAdmin,viewUsers)
 router.post('/',addUser)
 router.get('/:id',isAdmin,singleUser)
-router.get('/verify-email/:token',verifyEmail)
-router.get('/refresh-token',refresh)
+
 
 
 export default router
