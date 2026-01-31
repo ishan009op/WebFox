8760978c18fbc9abbfee955989d26af2fdf0e749
Ishan <ishanprashar97085@gmail.com>
2026-01-31 21:59:31 +0530

    functionality done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {HelmetProvider} from
 import Header from './Components/Header'
 import {Outlet} from 'react-router-dom'
 
